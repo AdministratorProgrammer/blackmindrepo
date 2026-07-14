689f24faca15a29d5873a18eb3558b68dd7117b1
AdministratorProgrammer <aleksandrsupp0rt@gmail.com>
2026-07-14 16:23:37 +0300

    efwegr4

--- main.py
+++ main.py
@@ -1,13 +1,4 @@
 from core.banner import glitch_banner_rich
 
-
-def main():
-    glitch_banner_rich("BLACKMIND")
-
-    # дальше запускается остальная программа
-    # ...
-    # твой код
-
-
 if __name__ == "__main__":
-    main()
+    glitch_banner_rich()
