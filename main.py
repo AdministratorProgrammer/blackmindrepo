ac08bc1711d21f5b78bc8bb606aaff0ca6ca3836
AdministratorProgrammer <aleksandrsupp0rt@gmail.com>
2026-07-14 16:11:13 +0300

    efweg

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 from core import banner
 
 if __name__ == "__main__":
-    from rich import print as rprint
-    rprint("[bold yellow]Начинаю глитч-анимацию...[/bold yellow]")
     banner.glitch_banner_rich("BLACKMIND")
